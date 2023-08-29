e8db2bace7799cc6edc75a264bdac61b445159ed
Lasse Vestergaard <lassevestergaard95@hotmail.com>
2023-08-29 10:52:56 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+const list = document.querySelector("ul");
+let counter = 0;
+const interval = setInterval(addLi, 1000);
+
+
+// det er vigtigt at de to nedenstående const står inde i funktionen så de generer et nyt random tal og li for hvert sekund(interval)
+function addLi() {
+  const randomTal = Math.floor(Math.random() * 100);
+  const li = document.createElement("li");
+
+  li.style.setProperty("--height", randomTal);
+  list.appendChild(li);
+  counter++;
+}
+
+
+// removeFirstLi();
+// function removeFirstLi(){
+// const firstLi = list.querySelector("li:first-child");
+// if (counter > 20){
+//     list.removeChild(firstLi);
+//     counter--
+//   }
+// }
