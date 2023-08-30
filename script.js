45824e5775efd7e777e12b94a7358898729dc843
Lasse Vestergaard <lassevestergaard95@hotmail.com>
2023-08-30 09:34:13 +0200

    opdateret med array

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 const list = document.querySelector("ul");
-let counter = 0;
-const interval = setInterval(addLi, 1000);
+const værdier = []
+setInterval(addLi, 5000);
 
 // det er vigtigt at de to nedenstående const står inde i funktionen så de generer et nyt random tal og li for hvert sekund(interval)
 function addLi() {
   const randomTal = Math.floor(Math.random() * 100);
+  værdier.push(randomTal)
   const li = document.createElement("li");
 
   li.style.setProperty("--height", randomTal);
   list.appendChild(li);
-  counter++;
-  if (list.children.length > 20) {
-    list.removeChild(list.children[0]);
-  }
+
+if (værdier.length > 20){
+  værdier.shift();
+  list.removeChild(list.children[0])
+}
+console.log(værdier)
 }
